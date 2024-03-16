6e79177c2da7d6fb4a7b16a66ec70621f555f7c5
MatthewPCope <matthew.p.cope@gmail.com>
2024-03-16 08:24:03 -0700

    added css

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -7,16 +7,14 @@ export default function TodoList({todos, deleteTodo, toggleTodo}) {
     return (
         <div>
             <ul>
-                
-                    {todos.map(
-                        todo=> {
-                            return (
-                                <TodoItem {...todo} key={todo.id} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>                                )
-                            }
-                            )}
-                
+                {todos.map(todo=> {
+                    return (
+                        <TodoItem {...todo} key={todo.id} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>                                )
+                    }
+                )}
             </ul>
         </div>
     )
 }
+
 
